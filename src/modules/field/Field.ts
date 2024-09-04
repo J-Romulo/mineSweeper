2eef83983465337ab63b73b135b6ba66f5ed417d
J-Romulo <joseromulo.10@hotmail.com>
2024-09-03 23:55:20 -0300

    feat: add methods related to coordinates selection on the field

--- src/modules/field/Field.ts
+++ src/modules/field/Field.ts
@@ -1,8 +1,9 @@
 import { BombPlacer } from "./BombPlacer";
 import { FieldCreator } from "./FieldCreator"
 import { DifficultyFactory } from "./difficultyCalculators/DifficultyFactory";
+import { IField } from "./interfaces/IField";
 
-export class Field {
+export class Field implements IField {
     private size: number
     private bombsTotal: number
     field: number[][]
@@ -19,5 +20,47 @@ export class Field {
         this.field = fieldCreator.createField(this.size);
 
         bombPlacer.placeBombsInField(this.field, this.bombsTotal)
+    }
+
+    selectPoint(x: number, y: number) {
+        const pointSelected = this.field[y][x]
+
+        if(pointSelected === -1) {
+            this.field[y][x] = -10
+            return false
+        }
+
+        this.checkAdjacentBombs(x, y)
+
+        return true
+    }
+
+    checkAdjacentBombs(x: number, y: number){
+        let totalBombsAdjacent = 0
+        let originalX = x
+        let originalY = y
+        for(y = originalY - 1; y < originalY + 2; y++){
+            for(x = originalX - 1; x < originalX + 2; x++){
+                if((y >= 0 && y < this.size) && (x >= 0 && x < this.size)){
+                    if(this.field[y][x] === -1) totalBombsAdjacent++
+                }
+            }
+        }
+
+        this.field[originalY][originalX] = totalBombsAdjacent === 0 ? -2 : totalBombsAdjacent
+
+        if(totalBombsAdjacent === 0){
+            for(y = originalY - 1; y < originalY + 2; y++){
+                for(x = originalX - 1; x < originalX + 2; x++){
+                    if((y >= 0 && y < this.size) && (x >= 0 && x < this.size)){
+                        if((x !== originalX || y !== originalY) && this.field[y][x] === 0){
+                            this.checkAdjacentBombs(x, y)
+                        }
+                    }
+                }
+            }
+        }
+
+        return totalBombsAdjacent
     }
 }
