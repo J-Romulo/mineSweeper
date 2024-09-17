98bbea7a20328e44068ebbb88ced5d5480380630
J-Romulo <joseromulo.10@hotmail.com>
2024-09-16 23:23:52 -0300

    refac: implement empty methods

--- src/application/UserInterface/Graphics/GraphicsInterface.ts
+++ src/application/UserInterface/Graphics/GraphicsInterface.ts
@@ -1,25 +1,36 @@
-import { QMainWindow } from "@nodegui/nodegui";
 import { Cell } from "../../../data/cell/Cell";
 import { IUserInterface } from "../IUserInterface";
 
 export class GraphicsInterface implements IUserInterface {
     mainMenu() {
 
-        const win = new QMainWindow();
+/*         const win = new QMainWindow();
 
         win.show();
-        (global as any).win = win;
+        (global as any).win = win; */
     }
 
     difficultyLevel() {
+    // TODO: Implement difficulty level logic
     }
-
+    
     field(field: Cell[][]) {
+    // TODO: Implement field rendering logic
     }
-
+    
     finalField(field: Cell[][]) {
+    // TODO: Implement final field rendering logic
     }
-
+    
     playOptions() {
+    // TODO: Implement play options logic
+    }
+    
+    getPlayerNickname() {
+    // TODO: Implement player nickname input logic
+    }
+    
+    displayLeaderboard(records: any[]) {
+    // TODO: Implement leaderboard display logic
     }
 }
